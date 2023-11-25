a7cd961790763cd1ba619be289f6f4ffbaaf44c9
LiamEdwards931 <>
2023-11-25 11:14:54 +0000

    add an approve comments function into the commentadmin class

--- django_blog_app/admin.py
+++ django_blog_app/admin.py
@@ -5,7 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-   
+ 
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
