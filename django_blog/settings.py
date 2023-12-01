1421d8ce93cdc3322988107b4d8dffcaa268bec1
LiamEdwards931 <>
2023-12-01 14:54:03 +0000

    deployment commit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     '8000-liamedwards931-django-bl-fjt6g4n74l.us2.codeanyapp.com',
